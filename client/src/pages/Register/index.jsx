0541a9ca0c34bb68305d17bdfecc53fe6813e2be
edimilson braz <edimilson.gt8@gmail.com>
2023-05-22 16:55:01 -0300

    :sparkles: Add useForm/yup / validando inputs

--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,178 +1,163 @@
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
+import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
-import {
-  checkName,
-  checkConfirmPassword,
-  checkEmail,
-  checkPassword
-} from '../../Scripts/validateForm'
-import styles from './styles.module.css'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
 import api from '../../service/api'
-import { ContainerError, ContainerRegister, ContentButton, ContentPassword, RegisterButton, RegisterContent, RegisterForm, RegisterInput, WrapperInput } from './style'
+import { ToastContainer, toast } from 'react-toastify'
+
+import {
+  ContainerRegister,
+  ContentButton,
+  ContentPassword,
+  RegisterContent,
+  RegisterForm,
+  RegisterInput,
+  WrapperInput
+} from './style'
 import { Button } from '../../components/Button'
 
+import styles from './styles.module.css'
+
+const schema = yup
+  .object({
+    name: yup.string().required('O nome é obrigatório!'),
+    lastName: yup.string().required('O sobrenome é obrigatório!'),
+    email: yup
+      .string()
+      .email('Digite um email válido')
+      .required('O email é obrigatório!'),
+    password: yup
+      .string()
+      .min(6, 'A senha deve ter pelo menos 6 digitos!')
+      .required('A senha é obrigatória!'),
+    confirmPassword: yup
+      .string()
+      .required('Confirmar a senha é obrigatório!')
+      .oneOf([yup.ref('password')], 'As senhas devem ser iguais')
+  })
+  .required()
+
 export function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
+
   const passwRef = useRef()
   const confirmPasswRef = useRef()
   const iconRef = useRef()
-  const nomeRef = useRef()
-  const sobrenomeRef = useRef()
-
-  const [email, setEmail] = useState('')
-
-  //Gerenciamento de erros do form com useState
-  const [firstname, setNome] = useState(false)
-  const [lastname, setSobrenome] = useState(false)
-  const [password, setPassword] = useState(false)
-  const [confirmPassword, setConfirmPassword] = useState(false)
-  const [emailError, setEmailError] = useState(false)
 
   const navigate = useNavigate()
 
   async function registerUser(userData) {
-    return await api.post('api/v1/auth/register', userData)
-  }
-
-  const handlerSubmit = async (event) => {
-    event.preventDefault()
-
-    const userData = {
-      firstname: nomeRef.current.value,
-      lastname: sobrenomeRef.current.value,
-      email: email,
-      password: passwRef.current.value
-    }
-
-    console.log(userData)
-
     try {
-      const response = await registerUser(userData)
-      navigate('/Login')
-      alert('Usuario cadastrado! Agora só logar.')
-      console.log(response) // Tratar a resposta do servidor aqui
-    } catch (error) {
-      console.log(error) // Tratar o erro aqui
-      alert('Erro ao se cadastrar  ' + error)
-    }
+      await api.post('api/v1/auth/register', userData).then((result) => {
+        if (result.status == 200) {
+          toast('Usuario cadastrado! Agora só logar., ', {
+            type: 'success',
+            autoClose: 2000
+          })
 
-    checkName(nomeRef.current.value) ? setNome(false) : setNome(true)
-    checkName(sobrenomeRef.current.value)
-      ? setSobrenome(false)
-      : setSobrenome(true)
-    checkEmail(email) ? setEmailError(false) : setEmailError(true)
-    checkPassword(passwRef.current.value)
-      ? setPassword(false)
-      : setPassword(true)
-    checkConfirmPassword(passwRef.current.value, confirmPasswRef.current.value)
-      ? setConfirmPassword(false)
-      : setConfirmPassword(true)
+          setTimeout(() => {
+            navigate('/Login')
+          }, 3000)
+        }
+      })
+    } catch (error) {
+      toast('Erro ao tentar Cadastrar usuário ' + error, {
+        type: 'error',
+        autoClose: 2000
+      })
+    }
   }
 
-  const showHide = () =>
-    //Função para visualização de senha do campo input
-    {
-      if (passwRef.current.type === 'password') {
-        passwRef.current.type = 'text'
-        iconRef.current.className = `${styles.hide}`
-      } else {
-        passwRef.current.type = 'password'
-        iconRef.current.className = ''
-      }
+  async function formData(dataForm) {
+    const userData = {
+      firstname: dataForm.name,
+      lastname: dataForm.lastName,
+      email: dataForm.email,
+      password: dataForm.password
     }
+
+    await registerUser(userData)
+  }
+
+  const showHide = () => {
+    //Função para visualização de senha do campo input
+    if (passwRef.current.type === 'password') {
+      passwRef.current.type = 'text'
+      iconRef.current.className = `${styles.hide}`
+    } else {
+      passwRef.current.type = 'password'
+      iconRef.current.className = ''
+    }
+  }
 
   return (
     <ContainerRegister>
       <h1>Criar conta</h1>
-      <RegisterForm>
+      <RegisterForm onSubmit={handleSubmit(formData)}>
         <RegisterContent>
           <WrapperInput>
-            <label htmlFor="name">Nome</label>
+            <label>Nome</label>
             <RegisterInput
-              className={firstname ? 'border-error' : ''}
-              ref={nomeRef}
+              {...register('name', { required: true })}
               type="text"
-              name=""
-              id="name"
-              required
             />
+            <span>{errors.name?.message}</span>
           </WrapperInput>
           <WrapperInput>
-            <label htmlFor="lastname">Sobrenome</label>
+            <label>Sobrenome</label>
             <RegisterInput
-              className={lastname ? 'border-error' : ''}
-              ref={sobrenomeRef}
+              {...register('lastName', { required: true })}
               type="text"
-              name=""
-              id="lastname"
-              required
             />
+            <span>{errors.lastName?.message}</span>
           </WrapperInput>
         </RegisterContent>
         <WrapperInput>
-          <label htmlFor="email">E-mail</label>
+          <label>E-mail</label>
           <RegisterInput
-            className={emailError ? 'border-error' : ''}
-            type="email"
-            name=""
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            required
+            {...register('email', { required: true })}
+            type="text"
           />
+          <span>{errors.email?.message}</span>
         </WrapperInput>
         <WrapperInput>
-          <label htmlFor="password">Senha</label>
+          <label>Senha</label>
           <ContentPassword>
             <RegisterInput
-              className={password ? 'border-error' : ''}
-              ref={passwRef}
+              {...register('password', { required: true })}
+              // ref={passwRef}
               type="password"
-              name=""
-              id="password"
-              required
             />
-
+            <span>{errors.password?.message}</span>
             <div ref={iconRef} id={styles.icon} onClick={showHide}></div>
           </ContentPassword>
         </WrapperInput>
         <WrapperInput>
-          <label htmlFor="confirmpassword">Confirmar Senha</label>
+          <label>Confirmar Senha</label>
           <RegisterInput
-            className={confirmPassword ? 'border-error' : ''}
-            ref={confirmPasswRef}
+            {...register('confirmPassword', { required: true })}
+            // ref={confirmPasswRef}
             type="password"
-            name=""
-            id="confirmpassword"
-            required
           />
+          <span>{errors.confirmPassword?.message}</span>
         </WrapperInput>
         <ContentButton>
-          <Button type="submit" onClick={handlerSubmit}>
-            Criar conta
-          </Button>
-          
+          <Button type="submit">Criar conta</Button>
+
           <span>
             Já tem uma conta? <Link to="/login">Iniciar sessão</Link>
           </span>
         </ContentButton>
       </RegisterForm>
-      {firstname || lastname || password || confirmPassword || emailError ? (
-        <ContainerError className={styles.containerError}>
-          <ul>
-            {firstname ? <li> * O nome digitado não é válido</li> : ''}
-            {lastname ? <li> * O sobrenome digitado não é válido</li> : ''}
-            {emailError ? <li> * E-mail digitado não é válido</li> : ''}
-            {password ? (
-              <li> * A senha deve ter mais de seis caracteres.</li>
-            ) : (
-              ''
-            )}
-            {confirmPassword ? <li> * As senhas devem ser idênticas</li> : ''}
-          </ul>
-        </ContainerError>
-      ) : (
-        ''
-      )}
+      <ToastContainer />
     </ContainerRegister>
   )
 }
--- client/src/pages/Register/style.js
+++ client/src/pages/Register/style.js
@@ -44,6 +44,11 @@ export const WrapperInput = styled.div`
     margin-bottom: 0.5rem;
   }
 
+  span {
+    color: red;
+    font-size: 0.875rem;
+  }
+
   @media screen and (max-width: 578px) {
     label {
       margin-top: 0.5rem;
@@ -59,19 +64,11 @@ export const RegisterInput = styled.input`
   box-shadow: 1px 1px 5px #00000034;
   padding: 5px;
   font-size: 1rem;
-
-  .border-error {
-    border: 12px;
-    background-color: rgba(255, 109, 109, 0.589);
-  }
 `
 
 export const ContentPassword = styled.div`
   position: relative;
 
-  div:nth-child(2) {
-    background-color: red;
-  }
 `
 
 export const ContentButton = styled.div`
@@ -81,22 +78,5 @@ export const ContentButton = styled.div`
 
   a {
     color: var(--primary);
-  }
-`
-
-export const ContainerError = styled.div`
-  width: 100%;
-  height: auto;
-  background-color: var(--white);
-  border: 2px solid rgba(255, 48, 48, 0.849);
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
-  color: red;
-  border-radius: 5px;
-
-  li {
-    margin: 5px;
-    list-style: none;
   }
 `
